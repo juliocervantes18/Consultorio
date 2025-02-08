16ea2b1fb1c6c159d01133ecbe0c34539aa46c6c
juliocervantes18 <julio18.cervantes@gmail.com>
2025-02-08 12:41:06 -0700

    rama para modificar codigo

--- src/Consultorio.java
+++ src/Consultorio.java
@@ -6,12 +6,19 @@ public class Consultorio implements BaseDatos {
     private List<Paciente> pacientes;
     private List<Cita> citas;
 
-    public Consultorio(){
+    public Consultorio() {
         doctores = new ArrayList<>();
         pacientes = new ArrayList<>();
         citas = new ArrayList<>();
     }
+
     public void agregarDoctor(Doctor doctor) {
         doctores.add(doctor);
 
+    }
+
+    @Override
+    public void guardardatos() {
+
+    }
 }
